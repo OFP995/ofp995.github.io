1c539e211786ec9bd60d0caefa0802a887722ab6
Dima <ofp995@mail.ru>
2021-10-08 15:30:57 +0300

    запрет добавки пустой строки

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const App = {
             placeholderString: 'Введите название заметки',
             title: 'Список заметок',
             inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2','Заметка 25']
+            notes: ['Заметка 1', 'Заметка 2', 'Заметка 25']
         }
     },
     // метод, где перечисляем обьекты и набор функции которые присутствуют в приложении
- methods: {
-     inputChangeHandler(event){
+    methods: {
+        inputChangeHandler(event) {
             // console.log('inputChangeHandler', event.target.value)   //путь где лежат данные что вводили в строке
-             this.inputValue = event.target.value     //this - обращение к data где лежат переменные и им присвается значение из пути куда записываются данный пользователь
-         },
-         addNewNote() {
-         this.notes.push(this.inputValue)
-             this.inputValue ='' // пустая строка после ввода информации
-     },
-     removeNote(idx) {
-         this.notes.splice(idx, 1)  // начинаем удаление с индекса idx и удаляем один элемент
-     }
- }
+            this.inputValue = event.target.value     //this - обращение к data где лежат переменные и им присвается значение из пути куда записываются данный пользователь
+        },
+        addNewNote() {
+            if(this.inputValue != '')//Если строка не пустая
+                this.notes.push(this.inputValue)//Тут в список переменной note добавляется введённая вользователем строка this.inputValue
+            this.inputValue = '' // пустая строка после ввода информации
+        },
+        removeNote(idx) {
+            this.notes.splice(idx, 1)  // начинаем удаление с индекса idx и удаляем один элемент
+        }
+    }
 }
 
 Vue.createApp(App).mount('#app')
