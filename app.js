0750aace06dad940983825a7321c12bbbf88a404
Dima <ofp995@mail.ru>
2021-10-09 09:40:45 +0300

    Залупа кентавра

--- app.js
+++ app.js
@@ -9,19 +9,40 @@ const App = {
     },
     // метод, где перечисляем обьекты и набор функции которые присутствуют в приложении
     methods: {
-        inputChangeHandler(event) {
-            // console.log('inputChangeHandler', event.target.value)   //путь где лежат данные что вводили в строке
-            this.inputValue = event.target.value     //this - обращение к data где лежат переменные и им присвается значение из пути куда записываются данный пользователь
-        },
+
         addNewNote() {
-            if(this.inputValue != '')//Если строка не пустая!
+            if (this.inputValue != '') {//Если строка не пустая!
                 this.notes.push(this.inputValue)//Тут в список переменной note добавляется введённая вользователем строка this.inputValue
-            this.inputValue = '' // пустая строка после ввода информации
+                this.inputValue = '' // пустая строка после ввода информации
+            }
+        },
+        doubleCount() {
+            console.log('doubleCount') // Не оптимизировано, вызывается при каждом вводе символа в строке
+            return this.notes.length * 2
+        },
+        toUpperCase(item) {//метод для постановки верхнего регистра у заметки
+            return item.toUpperCase()
         },
         removeNote(idx) {
             this.notes.splice(idx, 1)  // начинаем удаление с индекса idx и удаляем один элемент
         }
+    },
+    computed: {
+
+        doubleCountComputed() {
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {   // если длина вводимых символов > 10 то всё обнуляется
+                this.inputValue = ''
+            }
+            console.log('input Value changed', value) // мониторим что следиться за переменной
+        }
     }
+
 }
 
 Vue.createApp(App).mount('#app')
