23e9dac3545806e5dfe4700e357768825375973c
Dima <ofp995@mail.ru>
2021-10-10 20:57:34 +0300

    удаление строки по ее номеру

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
-const App = {
-    data() {      //данные
-        return {
-            placeholderString: 'Введите название заметки',
-            title: 'Список заметок',
-            inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
-        }
-    },
-    // метод, где перечисляем обьекты и набор функции которые присутствуют в приложении
-    methods: {
+Vue.createApp({
 
-        addNewNote() {
-            if (this.inputValue != '') {//Если строка не пустая!
-                this.notes.push(this.inputValue)//Тут в список переменной note добавляется введённая вользователем строка this.inputValue
-                this.inputValue = '' // пустая строка после ввода информации
-            }
+    data: () => ({
+        myHtml: '<h1>  удали строку по цифре </h1>',
+        title: 'Я есть грут',
+        person:{
+            firstName:'Dima',
+            lastName :'Teter',
+            age: 222
         },
-        doubleCount() {
-            console.log('doubleCount') // Не оптимизировано, вызывается при каждом вводе символа в строке
-            return this.notes.length * 2
-        },
-        toUpperCase(item) {//метод для постановки верхнего регистра у заметки
-            return item.toUpperCase()
-        },
-        removeNote(idx) {
-            this.notes.splice(idx, 1)  // начинаем удаление с индекса idx и удаляем один элемент
-        }
-    },
-    computed: {
-
-        doubleCountComputed() {
-            console.log('doubleCountComputed')
-            return this.notes.length * 2
-        },
-    },
-    watch: {
-        inputValue(value) {
-            if (value.length > 10) {   // если длина вводимых символов > 10 то всё обнуляется
-                this.inputValue = ''
-            }
-            console.log('input Value changed', value) // мониторим что следиться за переменной
-        }
-    }
-
-}
-
-Vue.createApp(App).mount('#app')
+        items:[1,2,3,4,5,6]
+    }),
+//     methods: {
+//         stopPropagation(event) {
+// event.stopPropagation()
+//    аналог
+//         }
+//     }
+}).mount('#app')
